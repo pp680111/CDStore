5bfc4cac26eb0d50db9c72579ab0879f69fda303
pp680111 <pp680111@163.com>
2017-02-18 16:08:24 +0800

    z

--- controller/Admin.php
+++ controller/Admin.php
@@ -2,10 +2,9 @@
     namespace controller;
     session_start();
 
-    class Index
-    {   
-        //显示index页
-        function index()
+    class Admin
+    {
+        function admin()
         {
             
         }
