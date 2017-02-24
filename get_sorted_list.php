52df982f69f7827f7347d82f3f47f647ab31eeb0
pp680111 <pp680111@163.com>
2017-02-24 20:27:39 +0800

    z

--- get_sorted_list.php
+++ get_sorted_list.php
@@ -1,5 +1,5 @@
 <?php
     require_once('CDStore_fns.php');
-    $result = get_sorted_list($_REQUEST['option'],$_REQUEST['page_index'],12);
+    $result = get_sorted_list($_REQUEST['option'],$_REQUEST['page_index'],$_REQUEST['page_size']);
     echo json_encode($result);
 ?>
